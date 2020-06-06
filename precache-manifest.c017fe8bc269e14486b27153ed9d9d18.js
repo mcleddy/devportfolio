6f5dacc7ae5b4f4fa802bca7dae9468713d41ec8
mcleddy <maryleddy1@gmail.com>
2020-06-06 02:09:21 -0700

    Updates

---
+++ precache-manifest.c017fe8bc269e14486b27153ed9d9d18.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "096f39e0ff31795e5c3e7b96c9d0622d",
+    "url": "/devportfolio/index.html"
+  },
+  {
+    "revision": "4f8f1f3ccaa15efcab0d",
+    "url": "/devportfolio/static/css/main.fb012007.chunk.css"
+  },
+  {
+    "revision": "f229326db4a6f9f1e093",
+    "url": "/devportfolio/static/js/2.6a1f973d.chunk.js"
+  },
+  {
+    "revision": "618d9e067b5f7c0ee048a43ae1109b7d",
+    "url": "/devportfolio/static/js/2.6a1f973d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f8f1f3ccaa15efcab0d",
+    "url": "/devportfolio/static/js/main.5eedf330.chunk.js"
+  },
+  {
+    "revision": "98b1cab3d1f7890e9135",
+    "url": "/devportfolio/static/js/runtime-main.08d527b5.js"
+  },
+  {
+    "revision": "95cb07ba6394608aff25f1cef1ab6927",
+    "url": "/devportfolio/static/media/CreationsPic.95cb07ba.png"
+  },
+  {
+    "revision": "f22817079927ebb5b62ffdea51d0fc05",
+    "url": "/devportfolio/static/media/DnDpic.f2281707.png"
+  },
+  {
+    "revision": "a0d31519cc5645b4d024ea4be518c28f",
+    "url": "/devportfolio/static/media/PACPic.a0d31519.png"
+  },
+  {
+    "revision": "79986cfe0ba58fca3871ceb56564c100",
+    "url": "/devportfolio/static/media/VolunTourPic.79986cfe.png"
+  },
+  {
+    "revision": "ac68e9ede5b84d83bf172fa518f03461",
+    "url": "/devportfolio/static/media/gif.ac68e9ed.gif"
+  },
+  {
+    "revision": "c5c502876072b029777af952de544fa2",
+    "url": "/devportfolio/static/media/linkedinBlack.c5c50287.png"
+  },
+  {
+    "revision": "8494e1c50edf68e6db46999194a7e7aa",
+    "url": "/devportfolio/static/media/logo.8494e1c5.png"
+  },
+  {
+    "revision": "f22817079927ebb5b62ffdea51d0fc05",
+    "url": "/devportfolio/static/media/motelPic.f2281707.png"
+  },
+  {
+    "revision": "8893b5420065c68dd8fdfbf6e74f53bc",
+    "url": "/devportfolio/static/media/mushroomPic.8893b542.png"
+  },
+  {
+    "revision": "c66d4dafe165775542d4f649d7aa1bea",
+    "url": "/devportfolio/static/media/profile.c66d4daf.png"
+  }
+]);
