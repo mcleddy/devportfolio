58103eb2284bbb57e4797b21ddc2720035fe13c3
mcleddy <maryleddy1@gmail.com>
2020-06-06 03:16:38 -0700

    m

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import registerServiceWorker from './serviceWorker';
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom";
 ReactDOM.render(
 <BrowserRouter>
     <App />,
