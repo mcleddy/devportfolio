1174f2cf7a50a4c6fd02d7826fef9b0488c004e7
mcleddy <maryleddy1@gmail.com>
2020-06-07 01:31:52 -0700

    a

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<App />, 
+document.getElementById('root'));
+
 registerServiceWorker();
